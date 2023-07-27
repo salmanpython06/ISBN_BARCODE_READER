0ae867a09b2066418abd955ce52a7e22e0e1c7d5
Salman Mallah <94690914+salmanpython06@users.noreply.github.com>
2023-07-27 23:00:25 +0500

    Add files via upload

--- code_reader.py
+++ code_reader.py
@@ -6,15 +6,14 @@ import cv2 # opencv
 import requests
 import numpy as np
 import pandas as pd
+import os 
 import pyzbar.pyzbar as pyzbar
+from csv import DictWriter
 
 isbn = ''
 cam = cv2.VideoCapture(0)
 
-global a
-a=0
-
-def datasave(a,isbn):
+def datasave(isbn):
     isbn = str(isbn)
     isbn = isbn[2:-1]
 
@@ -26,21 +25,33 @@ def datasave(a,isbn):
     data = r.json()
     try:
         book_info = data["items"][0]["volumeInfo"]
-
+        print(book_info)
         title = book_info.get("title")
         authors = book_info.get("authors")
         publish_date = book_info.get("publishedDate")
-        isbn = book_info.get("industryIdentifiers")[1]["identifier"]
+        isbn = book_info.get("industryIdentifiers")[0]["identifier"]
         description = book_info.get("description")
         print("\n\n\n")
+
         table = {"title": title, "authors": authors, "isbn": isbn, "publish_date": publish_date, "description": description}
-        # print(table)
 
         df = pd.DataFrame(table)
-        df.iloc[a:]=df
-        print(df.iloc[a:])
-        print(a)
-        df.to_csv('data.csv', mode='a', header=False, index=False)
+        print(df)
+
+        
+        with open('book_data.csv', 'a', newline="") as wf:
+            dict_writer = DictWriter(wf, fieldnames=['title', 'authors', 'isbn', 'publish_date', 'description'])
+            if os.stat('book_data.csv').st_size == 0:
+                dict_writer.writeheader()
+            else:
+                read_csv = pd.read_csv("book_data.csv")
+                check_isbn = read_csv["isbn"].tolist()
+                # print(check_isbn)
+                if isbn in check_isbn:
+                    print("Book is already registered...")
+                else:
+                    print(f"this isbn: {isbn} not matched")
+                    df.to_csv('book_data.csv', mode='a', header=False, index=False)
     except KeyError:
         print("Invalid ISBN\nPlease Try again!")
  
@@ -50,8 +61,8 @@ while True:
     decodedObjects = pyzbar.decode(frame)
     for obj in decodedObjects:
         isbn = obj.data
-        a=a+1
-        datasave(a,isbn)
+
+        datasave(isbn)
     cv2.imshow("Scan QR code Prehss ESC to quite.", frame)
     key = cv2.waitKey(1)
 
